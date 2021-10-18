48123b500b1f5720346cda15751e5f5181fb2c51
Harsh Tiwari <harsh.tiwari1995@gmail.com>
2021-10-19 00:10:53 +0530

    fc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import './App.css'
+import Pokedex from './Components/Pokedex'
+const App = () => {
+  const [pokemon, setPokemon] = useState([])
+  const [search, setSearch] = useState('')
 
-function App() {
+  const fetchPokemon = async () => {
+    const data = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=151`)
+
+    // console.log(data.data)
+    const createPokemon = (result) => {
+      result.forEach(async (el) => {
+        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${el.name}`)
+        
+        setPokemon(current => [...current, res.data])
+      })
+    }
+    createPokemon(data.data.results)
+
+  }
+  useEffect(() => {
+    fetchPokemon()
+  }, [])
+
+
+  const handleChange = (e) => {
+    setSearch(e.target.value)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='main'>
+      <div className='header'>
+        <h1>Pokedex</h1>
+        <input type="text" placeholder='Search Pokemon...' value={search} onChange={handleChange} />
+      </div>
+      <div className="pokemon">
+        {pokemon.filter(el => {
+          if (el.name.toLowerCase().includes(search.toLowerCase()))
+            return el
+            return null
+        }).map((el) => {
+          return (
+            <Pokedex
+              index={el.id}
+              name={el.name}
+              image={el.sprites.other.dream_world.front_default}
+              key={el.name}
+              type={el.types}
+              weight={el.weight}
+              height={el.height}
+              ability={el.abilities}
+            // stype={el.types[1].type.name}
+            />
+          )
+        })}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Components/Pokedex.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './pokedex.css'
+const Pokedex = ({ name, image, index, type, weight, height,ability }) => {
+    const style = `card ${type}`
+    return (
+        <div className="container">
+            <div className={style}>
+                <div className="front">
+                <img src={image} alt={name}/>
+                <div className='name'>{name}</div>
+                </div>
+                <div className="back">
+                    {index<10?`#00${index}`:`#0${index}`}
+                    <div>Type: {type[0].type.name}</div>
+                    <div>Weight:{weight}</div>
+                    <div>Height:{height}</div>
+                    <div>Ability:{ability.map((el,id)=>(<span key={id}>{el.ability.name}.</span>))}</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Pokedex
